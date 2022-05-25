f0d60560d44d3e9a1c52d6b13050e94ce17fb281
Jose Carlos Coronado <83930715+J-Carlos-Coronado@users.noreply.github.com>
2022-05-24 22:22:53 -0500

    Update Resolucion1K.java

--- TvDemo/src/PResoluciones/Resolucion1K.java
+++ TvDemo/src/PResoluciones/Resolucion1K.java
@@ -1,15 +1,20 @@
 package PResoluciones;
 
-
-
+// @author: Jose Carlos Coronado Chable (Documentador)
+// @author:Alex Reyes (Coder)
+//@version: 24/05/2022
 /**
  * Class Resolucion1K
+ //Esat clase nos dara la resolucion 1k
  */
+//Clase ResolucionHD es una clase hija de Resolucion 
 public class Resolucion1K extends Resolucion {
 
   //
   // Fields
   //
+  // Campos de tipo privado que tienen como variables estaticas largo y ancho que sirven para dar un tamaño a la pantalla 
+  //Largo=1080 x ancho= 1920
     private final static int LARGO = 1080;
     private final static int ANCHO = 1920;
      
@@ -17,7 +22,7 @@ public class Resolucion1K extends Resolucion {
   //
   // Fields
   //
-    
+  //Super toma como parametros el largo y el ancho 
     public Resolucion1K() {
         super(LARGO,ANCHO);
     }
@@ -45,4 +50,4 @@ public class Resolucion1K extends Resolucion {
 
 
 
-}
+}//Cierre de la clase y del metodo
