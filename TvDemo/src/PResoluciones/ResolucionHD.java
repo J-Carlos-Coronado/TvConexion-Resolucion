39b2a550b98981919a2cd0e59db2d5014d2e2558
Jose Carlos Coronado <83930715+J-Carlos-Coronado@users.noreply.github.com>
2022-05-24 21:52:55 -0500

    Update ResolucionHD.java

--- TvDemo/src/PResoluciones/ResolucionHD.java
+++ TvDemo/src/PResoluciones/ResolucionHD.java
@@ -1,10 +1,15 @@
 package PResoluciones;
 
 
+// @author: Jose Carlos Coronado Chable (Documentador)
+// @author: Alex Reyes (Coder)
+
+//@version: 24/05/2022
 
 /**
  * Class ResolucionHD
  */
+//Clase ResolucionHD es una clase hija de Resolucion 
 public class ResolucionHD extends Resolucion {
 
 
@@ -12,13 +17,14 @@ public class ResolucionHD extends Resolucion {
     
   //
   // Fields
-  //
+  // Campos de tipo privado que tienen como variables estaticas largo y ancho que sirven para dar un tamaño a la pantalla 
     private final static int LARGO = 1280;
     private final static int ANCHO = 720;
     
   //
   // Constructors
-  //
+  // Creacion del constructor Resolucion HD que nos dara una mejor visivilidad en la tv 
+  //Super toma como parametros el largo y el ancho 
     public ResolucionHD() {
         super(LARGO,ANCHO);
     }
@@ -37,4 +43,4 @@ public class ResolucionHD extends Resolucion {
   // Other methods
   //
 
-}
+}//Cierre de la clase y del metodo
